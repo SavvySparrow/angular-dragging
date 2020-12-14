9403520ce29e01238b271a337bdf87a26fffcbe5
SavvySparrow <savvysparrow@protonmail.com>
2020-12-14 15:39:22 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent
   initGenerateSquare2dArray() {
     this.sqaure2DArray = [];
     let tempQuad: any;
-    const randomInt = this.getRandomInt(2, 10);
+    const randomInt = this.getRandomInt(9, 20);
     [...Array(randomInt)].forEach((_, i) => {
       tempQuad = this.getXYQuad();
       this.sqaure2DArray.push({
@@ -73,18 +73,17 @@ export class AppComponent
         currentBounds: {}
       });
     });
-    //console.log(this.sqaure2DArray.length, this.sqaure2DArray);
   }
 
   getXYQuad() {
     let found: boolean = false;
     let maxBoundX = (this.boundaryRef.nativeElement as HTMLElement).offsetWidth - 102;
     let maxBoundY = (this.boundaryRef.nativeElement as HTMLElement).offsetHeight - 102;
-    let cordX: number = this.getRandomInt(0, maxBoundX);
-    let cordY: number = this.getRandomInt(0, maxBoundY);
+    let cordX: number;
+    let cordY: number;
     let i = 0;
     this.ngZone.runOutsideAngular(() => {
-      while (!found && i<100) {
+      while (!found) {
         cordX = this.getRandomInt(0, maxBoundX);
         cordY = this.getRandomInt(0, maxBoundY);
         if (this.sqaure2DArray.length == 0) {
@@ -92,11 +91,13 @@ export class AppComponent
           found = true;
         } else {
           let foundTemp = true;
-          console.log(`verify Quad - (${cordX},${cordY}) and length - ${this.sqaure2DArray.length}`);
+          console.log(`do verify Quad - (${cordX},${cordY}) and length - ${this.sqaure2DArray.length}`);
           this.sqaure2DArray.every((item, index) => {
-            console.log(index,item.initialLeft,(cordX >= 0 && cordX < item.initialLeft-102),(cordX <= maxBoundX && cordX > item.initialLeft+102))
-            if (!((cordX >= 0 && cordX < item.initialLeft-102) || (cordX <= maxBoundX && cordX > item.initialLeft+102))
-            || !((cordY >= 0 && cordY < item.initialTop-102) || (cordY <= maxBoundY && cordY > item.initialTop+102))) {
+            console.log(index,item.initialLeft,item.initialTop,"X",(cordX >= 0 && cordX < item.initialLeft-102),(cordX <= maxBoundX && cordX > item.initialLeft+102),"Y",(cordY >= 0 && cordY < item.initialTop-102),(cordY <= maxBoundY && cordY > item.initialTop+102))
+            if (!(
+              (cordX >= 0 && cordX < item.initialLeft-102) || (cordX <= maxBoundX && cordX > item.initialLeft+102)
+              ||  (cordY >= 0 && cordY < item.initialTop-102) || (cordY <= maxBoundY && cordY > item.initialTop+102)
+              )) {
               foundTemp=false;
               console.log('break loop');
               return false;
@@ -112,56 +113,13 @@ export class AppComponent
       }
     });
 
-    //console.log(found, cord);
     return {x: cordX,y: cordY};
-  }
-
-  getRandomLeft(): number {
-    let found: boolean = false;
-    let maxBound =
-      (this.boundaryRef.nativeElement as HTMLElement).offsetWidth - 102;
-    //maxBound = 1444;
-    let cord: number = this.getRandomInt(0, maxBound);
-    let i = 0;
-    this.ngZone.runOutsideAngular(() => {
-      while (!found && i<100) {
-        cord = this.getRandomInt(0, maxBound);
-        if (this.sqaure2DArray.length == 0) {
-          console.log("found at",cord);
-          found = true;
-        } else {
-          let foundTemp = true;
-          console.log("length",this.sqaure2DArray.length,cord);
-          this.sqaure2DArray.every((item, index) => {
-            console.log(index,item.initialLeft,(cord >= 0 && cord < item.initialLeft-102),(cord <= maxBound && cord > item.initialLeft+102))
-            if (!((cord >= 0 && cord < item.initialLeft-102) || (cord <= maxBound && cord > item.initialLeft+102))) {
-              foundTemp=false;
-              console.log('break loop');
-              return false;
-            }
-            return true;
-          });
-          if(foundTemp) {
-            //console.log("found at",cord);
-            found=true;
-          } 
-        }
-        i++;
-      }
-    });
-
-    //console.log(found, cord);
-    return cord;
-  }
-
-  getRandomTop(): number {
-    return this.getRandomInt(0, 553);
   }
 
   getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum and minimun is inclusive
   }
 
   updateCurrentBounds(sqaure2D, cord) {
